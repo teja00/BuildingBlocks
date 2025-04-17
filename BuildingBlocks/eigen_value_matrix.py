3475ffc47f490a9ecde55b874f4cf7f4597e1ae8
teja00 <tejanagubandi00@gmail.com>
2025-04-17 08:37:05 -0500

    minor changes to doc

--- BuildingBlocks/eigen_value_matrix.py
+++ BuildingBlocks/eigen_value_matrix.py
@@ -9,7 +9,7 @@ import numpy as np
 
 # %% ../nbs/05_eigen_value_matrix.ipynb 4
 def calculate_eigenvalues_2by2(matrix: list[list[Union[float,int]]] # matrix of size 2 x 2
-						  ) -> list[float]: # eigenvalues of the matrix 2 x 1
+						  ) -> list[float]: # eigenvalues of the matrix 1 x 2
     trace = matrix[0][0] + matrix[1][1]
     det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     sqrt_value = np.sqrt(trace ** 2 - 4 * det)
@@ -18,8 +18,8 @@ def calculate_eigenvalues_2by2(matrix: list[list[Union[float,int]]] # matrix of 
     return [eigen1, eigen2]
 
 # %% ../nbs/05_eigen_value_matrix.ipynb 6
-def calculate_eigenvalues(matrix: list[list[Union[float,int]]] # matrix of size 2 x 2
-						  ) -> list[float]: # result of size n x 1
+def calculate_eigenvalues(matrix: list[list[Union[float,int]]] # matrix of size n x n
+						  ) -> list[float]: # result of size 1 x n
     np_matrix = np.array(matrix)
     if np_matrix.shape[0] != np_matrix.shape[1]:
         raise ValueError("Eigenvalues are only defined for square matrices.")
